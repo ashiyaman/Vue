ed4d112e7d0f7d4d8748007ab5f895e7bca8a446
ashiyaman <ashiya1005@gmail.com>
2017-07-20 12:53:52 +1000

    attribute and class binding

--- 3. Attribute and Class Binding/index.js
+++ 3. Attribute and Class Binding/index.js
@@ -1,6 +1,17 @@
 new Vue({
   el: '#vue-app',
   data: {
-    
+    btnTitle: 'Title from Vue',
+    className: 'color-red',
+    isLoading: false,
+    isEnabled: false
+  },
+  methods: {
+    toggleClass() {
+      this.isLoading = true
+    },
+    buttonEnable() {
+      this.isEnabled = true
+    }
   }
 })
