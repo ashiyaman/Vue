cf666b661d9e7aa9c375b0793b906f341ef9843a
ashiyaman <ashiya1005@gmail.com>
2017-07-20 14:33:42 +1000

    computed properties

--- 4. Computed Properties/index.js
+++ 4. Computed Properties/index.js
@@ -1,6 +1,24 @@
 new Vue({
   el: '#vue-app',
   data: {
-    
+    tasks: [
+      {description: 'Go to store', completed: true},
+      {description: 'Pay bills', completed: false},
+      {description: 'Complete vue tutorial', completed: false},
+      {description: 'Meeting with NGO', completed: false},
+      {description: 'Buy tickets', completed: false},
+      {description: 'Complete application', completed: true}
+    ]
+  },
+  //If we want to do some manipulation before rendering to DOM
+  computed: {
+    incompleteTasks() {
+      return this.tasks.filter(task => !task.completed)
+    }
+  },
+  methods: {
+    updateTask(task) {
+      task.completed = true
+    }
   }
 })
